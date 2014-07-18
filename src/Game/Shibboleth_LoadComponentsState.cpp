4b7d8bc6c7fdd128a6a454bb7c0358f0e822217a
Connway <2028855+Connway@users.noreply.github.com>
2014-07-17 23:11:22 -0700

    Added LuaLoader to the CreateResourceLoadersState.

--- src/Game/Shibboleth_LoadComponentsState.cpp
+++ src/Game/Shibboleth_LoadComponentsState.cpp
@@ -20,11 +20,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ************************************************************************************/
 
-#include "Shibboleth_LoadComponentsState.h"
-#include "Shibboleth_ComponentManager.h"
-#include "Shibboleth_String.h"
-#include "Shibboleth_App.h"
-#include <Gaff_DynamicModule.h>
+#include <Shibboleth_LoadComponentsState.h>
+#include <Shibboleth_ComponentManager.h>
+#include <Shibboleth_String.h>
+#include <Shibboleth_App.h>
 #include <Gaff_Utils.h>
 
 NS_SHIBBOLETH
