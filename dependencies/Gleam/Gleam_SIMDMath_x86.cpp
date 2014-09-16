2a228e2b4f9b0d158b0d963dea4c8384617a2790
Connway <2028855+Connway@users.noreply.github.com>
2014-09-15 22:58:05 -0700

    Updated Gleam.

--- dependencies/Gleam/Gleam_SIMDMath_x86.cpp
+++ dependencies/Gleam/Gleam_SIMDMath_x86.cpp
@@ -24,7 +24,6 @@ THE SOFTWARE.
 	defined(__i686__) || defined(_M_IX86) || defined(_X86_)
 
 #include "Gleam_SIMDMath_x86.h"
-#include <emmintrin.h>
 #include <intrin.h>
 
 NS_GLEAM
@@ -75,6 +74,15 @@ const SIMDType gSinCoeffs0 = { -0.16666667f, +0.0083333310f, -0.00019840874f, +2
 const SIMDType gSinCoeffs1 = { -2.3889859e-08f, -0.16665852f, +0.0083139502f, -0.00018524670f };
 const SIMDType gCosCoeffs0 = { -0.5f, +0.041666638f, -0.0013888378f, +2.4760495e-05f };
 const SIMDType gCosCoeffs1 = { -2.6051615e-07f, -0.49992746f, +0.041493919f, -0.0012712436f };
+
+const SIMDTransform gTransformIdentity = {
+	gWAxis, gWAxis, gOne
+};
+
+const SIMDMatrix gMatrixIdentity = {{
+	gXAxis, gYAxis,
+	gZAxis, gWAxis
+}};
 
 typedef float (*GetElementFunc)(const SIMDType&);
 typedef void (*SetElementFunc)(SIMDType&, float);
@@ -142,9 +150,9 @@ SIMDType SIMDPermute(const SIMDType& left, const SIMDType& right, unsigned int x
 
 SIMDType SIMDRound(const SIMDType& vec)
 {
-	static __m128i fraction_test = _mm_castps_si128(_mm_set1_ps(8388608.0f));
+	static SIMDTypei fraction_test = _mm_castps_si128(_mm_set1_ps(8388608.0f));
 
-	__m128i test = _mm_and_si128(_mm_castps_si128(vec), _mm_castps_si128(gSignMask));
+	SIMDTypei test = _mm_and_si128(_mm_castps_si128(vec), _mm_castps_si128(gSignMask));
 	test = _mm_cmplt_epi32(test, fraction_test);
 
 	SIMDType temp = _mm_cvtepi32_ps(_mm_cvtps_epi32(vec));
@@ -293,7 +301,7 @@ SIMDType SIMDCos(const SIMDType& vec)
 
 SIMDType SIMDATan2(const SIMDType& y, const SIMDType& x)
 {
-	__m128i tempi = _mm_set1_epi32(-1);
+	SIMDTypei tempi = _mm_set1_epi32(-1);
 	SIMDType valid_result = ((SIMDType*)&tempi)[0];
 	SIMDType pi = gPi;
 	SIMDType piOverTwo = gPiOverTwo;
@@ -305,7 +313,7 @@ SIMDType SIMDATan2(const SIMDType& y, const SIMDType& x)
 	SIMDType x_positive = _mm_and_ps(x, gNegZero);
 
 	{
-		__m128i temp = _mm_cmpeq_epi32(_mm_castps_si128(x_positive), _mm_castps_si128(gZero));
+		SIMDTypei temp = _mm_cmpeq_epi32(_mm_castps_si128(x_positive), _mm_castps_si128(gZero));
 		x_positive = ((SIMDType*)(&temp))[0];
 	}
 
@@ -623,6 +631,503 @@ SIMDType SIMDLoadAligned(float* buffer)
 SIMDType SIMDLoad(float* buffer)
 {
 	return _mm_load_ps(buffer);
+}
+
+SIMDType SIMDQuatMul(const SIMDType& left, const SIMDType& right)
+{
+	static const SIMDType controlWZYX = { 1.0f, -1.0f, 1.0f, -1.0f };
+	static const SIMDType controlZWXY = { 1.0f, 1.0f, -1.0f, -1.0f };
+	static const SIMDType controlYXWZ = { -1.0f, 1.0f, 1.0f, -1.0f };
+
+	SIMDType x = _mm_shuffle_ps(left, left, _MM_SHUFFLE(0, 0, 0, 0));
+	SIMDType y = _mm_shuffle_ps(left, left, _MM_SHUFFLE(1, 1, 1, 1));
+	SIMDType z = _mm_shuffle_ps(left, left, _MM_SHUFFLE(2, 2, 2, 2));
+	SIMDType temp1 = _mm_shuffle_ps(left, left, _MM_SHUFFLE(3, 3, 3, 3));
+
+	temp1 = _mm_mul_ps(temp1, right);
+
+	SIMDType temp2 = _mm_shuffle_ps(right, right, _MM_SHUFFLE(0, 1, 2, 3));
+	x = _mm_mul_ps(x, temp2);
+	x = _mm_mul_ps(x, controlWZYX);
+
+	temp2 = _mm_shuffle_ps(temp2, temp2, _MM_SHUFFLE(2, 3, 0, 1));
+	y = _mm_mul_ps(y, temp2);
+	y = _mm_mul_ps(y, controlZWXY);
+
+	temp2 = _mm_shuffle_ps(temp2, temp2, _MM_SHUFFLE(0, 1, 2, 3));
+	z = _mm_mul_ps(z, temp2);
+	z = _mm_mul_ps(z, controlYXWZ);
+
+	temp1 = _mm_add_ps(temp1, x);
+	temp1 = _mm_add_ps(temp1, y);
+	temp1 = _mm_add_ps(temp1, z);
+
+	return temp1;
+}
+
+SIMDType SIMDQuatConjugate(const SIMDType& quaternion)
+{
+	return _mm_mul_ps(quaternion, gNegOneXYZ);
+}
+
+SIMDType SIMDQuatInverse(const SIMDType& quaternion)
+{
+	SIMDType scale = SIMDVec4ReciprocalLengthSquared(quaternion);
+	SIMDType quat = SIMDQuatConjugate(quaternion);
+	return _mm_mul_ps(quat, scale);
+}
+
+SIMDType SIMDQuatGetAxis(const SIMDType& quaternion)
+{
+	return _mm_and_ps(quaternion, gXYZMask);
+}
+
+SIMDType SIMDQuatGetAngle(const SIMDType& quaternion)
+{
+	SIMDType w = _mm_shuffle_ps(quaternion, quaternion, _MM_SHUFFLE(3, 3, 3, 3));
+	return SIMDMul(gTwo, SIMDACos(w));
+}
+
+SIMDType SIMDQuatSlerp(const SIMDType& left, const SIMDType& right, const SIMDType& t)
+{
+	static const SIMDType eps = { 1.0f - 0.00001f, 1.0f - 0.00001f, 1.0f - 0.00001f, 1.0f - 0.00001f };
+	static const SIMDType sign_x = { 0x80000000, 0x00000000, 0x00000000, 0x00000000 };
+
+	SIMDType cos_theta = SIMDVec4Dot(left, right);
+	SIMDType control = _mm_cmplt_ps(cos_theta, gZero);
+	SIMDType sign = _mm_or_ps(_mm_andnot_ps(control, gOne), _mm_and_ps(gNegOne, control));
+
+	cos_theta = _mm_mul_ps(cos_theta, sign);
+	control = _mm_cmplt_ps(cos_theta, eps);
+
+	SIMDType sin_theta = _mm_mul_ps(cos_theta, cos_theta);
+	sin_theta = _mm_sub_ps(gOne, sin_theta);
+	sin_theta = _mm_sqrt_ps(sin_theta);
+
+	SIMDType theta = SIMDATan2(sin_theta, cos_theta);
+	SIMDType v = _mm_shuffle_ps(t, t, _MM_SHUFFLE(2, 3, 0, 1));
+	v = _mm_and_ps(v, gXYMask);
+	v = _mm_xor_ps(v, sign_x);
+	v = _mm_and_ps(v, gXAxis);
+
+	SIMDType s0 = _mm_mul_ps(v, theta);
+	s0 = SIMDSin(s0);
+	s0 = _mm_div_ps(s0, sin_theta);
+	s0 = _mm_or_ps(_mm_andnot_ps(control, v), _mm_and_ps(s0, control));
+
+	SIMDType s1 = _mm_shuffle_ps(s0, s0, _MM_SHUFFLE(1, 1, 1, 1));
+	s0 = _mm_shuffle_ps(s0, s0, _MM_SHUFFLE(0, 0, 0, 0));
+	s1 = _mm_mul_ps(s1, sign);
+	s1 = _mm_mul_ps(s1, right);
+
+	return _mm_add_ps(_mm_mul_ps(left, s0), s1);
+}
+
+SIMDType SIMDQuatTransform(const SIMDType& quaternion, const SIMDType& vec)
+{
+	SIMDType q_conj = SIMDQuatConjugate(quaternion);
+	SIMDType q = _mm_and_ps(gXYZMask, quaternion);
+
+	q = SIMDQuatMul(quaternion, vec);
+	q = SIMDQuatMul(q, q_conj);
+
+	return _mm_and_ps(q, gXYZMask);
+}
+
+SIMDType SIMDQuatFromAxisAngle(const SIMDType& axis, const SIMDType& angle)
+{
+	SIMDType a = _mm_mul_ps(gTwo, angle);
+	SIMDType v = _mm_or_ps(_mm_and_ps(axis, gXYZMask), gWAxis); // v = [x, y, z, 1]
+	SIMDType s = SIMDSin(a);
+
+	// s = [sin,sin,sin,cos]
+	s = _mm_and_ps(s, gXYZMask);
+	s = _mm_or_ps(s, _mm_and_ps(SIMDCos(a), gWMask));
+
+	return _mm_mul_ps(v, s);
+}
+
+SIMDType SIMDQuatFromAngles(const SIMDType& angles)
+{
+	static SIMDType sign = { 1.0f, -1.0f, -1.0f, 1.0f };
+
+	SIMDType vec = _mm_mul_ps(gHalf, angles);
+	SIMDType sin_angles = SIMDSin(vec);
+	SIMDType cos_angles = SIMDCos(vec);
+
+	SIMDType p0 = SIMDPermute(sin_angles, cos_angles, 0, 4, 4, 4);
+	SIMDType y0 = SIMDPermute(sin_angles, cos_angles, 5, 1, 5, 5);
+	SIMDType r0 = SIMDPermute(sin_angles, cos_angles, 6, 6, 2, 6);
+	SIMDType p1 = SIMDPermute(cos_angles, sin_angles, 0, 4, 4, 4);
+	SIMDType y1 = SIMDPermute(cos_angles, sin_angles, 5, 1, 5, 5);
+	SIMDType r1 = SIMDPermute(cos_angles, sin_angles, 6, 6, 2, 6);
+
+	SIMDType q0 = _mm_mul_ps(p0, y0);
+	SIMDType q1 = _mm_mul_ps(p1, sign);
+	q0 = _mm_mul_ps(q0, r0);
+	q1 = _mm_mul_ps(q1, y1);
+
+	return _mm_add_ps(_mm_mul_ps(q1, r1), q0);
+}
+
+SIMDMatrix SIMDQuatToMatrix(const SIMDType& quaternion)
+{
+	SIMDType q0 = _mm_add_ps(quaternion, quaternion);
+	SIMDType q1 = _mm_mul_ps(q0, quaternion);
+	SIMDType v0 = _mm_shuffle_ps(q1, q1, _MM_SHUFFLE(3, 0, 0, 1));
+	SIMDType v1 = _mm_shuffle_ps(q1, q1, _MM_SHUFFLE(3, 1, 2, 2));
+	v0 = _mm_and_ps(v0, gXYZMask);
+	v1 = _mm_and_ps(v1, gXYZMask);
+	SIMDType r0 = _mm_sub_ps(gOneXYZ, v0);
+	r0 = _mm_sub_ps(r0, v1);
+
+	v0 = _mm_shuffle_ps(quaternion, quaternion, _MM_SHUFFLE(3, 1, 0, 0));
+	v1 = _mm_shuffle_ps(q0, q0, _MM_SHUFFLE(3, 2, 1, 2));
+	v0 = _mm_mul_ps(v0, v1);
+
+	v1 = _mm_shuffle_ps(quaternion, quaternion, _MM_SHUFFLE(3, 3, 3, 3));
+	SIMDType v2 = _mm_shuffle_ps(q0, q0, _MM_SHUFFLE(3, 0, 2, 1));
+	v1 = _mm_mul_ps(v1, v2);
+
+	SIMDType r1 = _mm_add_ps(v0, v1);
+	SIMDType r2 = _mm_sub_ps(v0, v1);
+
+	v0 = _mm_shuffle_ps(r1, r2, _MM_SHUFFLE(1, 0, 2, 1));
+	v0 = _mm_shuffle_ps(v0, v0, _MM_SHUFFLE(1, 3, 2, 0));
+	v1 = _mm_shuffle_ps(r1, r2, _MM_SHUFFLE(2, 2, 0, 0));
+	v1 = _mm_shuffle_ps(v1, v1, _MM_SHUFFLE(2, 0, 2, 0));
+
+	q1 = _mm_shuffle_ps(r0, v0, _MM_SHUFFLE(1, 0, 3, 0));
+	q1 = _mm_shuffle_ps(q1, q1, _MM_SHUFFLE(1, 3, 2, 0));
+
+	SIMDType col1, col2, col3, col4;
+	col1 = q1;
+
+	q1 = _mm_shuffle_ps(r0, v0, _MM_SHUFFLE(3, 2, 3, 1));
+	q1 = _mm_shuffle_ps(q1, q1, _MM_SHUFFLE(1, 3, 0, 2));
+	col2 = q1;
+
+	col3 = _mm_shuffle_ps(v1, r0, _MM_SHUFFLE(3, 2, 1, 0));
+	col4 = gWAxis;
+
+	SIMDMatrix matrix = { {col1, col2, col3, col4} };
+
+	return matrix;
+}
+
+SIMDType SIMDQuatFromMatrix(const SIMDMatrix& matrix)
+{
+	static const SIMDType sign1 = { +1.0f, -1.0f, -1.0f, +1.0f };
+	static const SIMDType sign2 = { -1.0f, +1.0f, -1.0f, +1.0f };
+	static const SIMDType sign3 = { -1.0f, -1.0f, +1.0f, +1.0f };
+
+	SIMDType r0 = matrix.elements[0];
+	SIMDType r1 = matrix.elements[1];
+	SIMDType r2 = matrix.elements[3];
+	SIMDType r00 = _mm_shuffle_ps(r0, r0, _MM_SHUFFLE(0, 0, 0, 0));
+	SIMDType r11 = _mm_shuffle_ps(r1, r1, _MM_SHUFFLE(1, 1, 1, 1));
+	SIMDType r22 = _mm_shuffle_ps(r2, r2, _MM_SHUFFLE(2, 2, 2, 2));
+
+	SIMDType r11sr00 = _mm_sub_ps(r11, r00);
+	SIMDType r11sr00_le = _mm_cmple_ps(r11sr00, gZero);
+
+	SIMDType r11pr00 = _mm_add_ps(r11, r00);
+	SIMDType r11pr00_le = _mm_cmple_ps(r11pr00, gZero);
+
+	SIMDType r22_le = _mm_cmple_ps(r22, gZero);
+
+	SIMDType t0 = _mm_mul_ps(sign1, r00);
+	SIMDType t1 = _mm_mul_ps(sign2, r11);
+	SIMDType t2 = _mm_mul_ps(sign3, r22);
+
+	SIMDType temp1 = _mm_add_ps(t0, t1);
+	temp1 = _mm_add_ps(t2, temp1);
+	temp1 = _mm_add_ps(gOne, temp1);
+
+	t0 = _mm_shuffle_ps(r0, r1, _MM_SHUFFLE(1, 2, 2, 1));
+	t1 = _mm_shuffle_ps(r1, r2, _MM_SHUFFLE(1, 0, 0, 0));
+	t1 = _mm_shuffle_ps(t1, t1, _MM_SHUFFLE(1, 3, 2, 0));
+
+	SIMDType temp2 = _mm_add_ps(t0, t1);
+
+	t0 = _mm_shuffle_ps(r2, r1, _MM_SHUFFLE(0, 0, 0, 1));
+	t1 = _mm_shuffle_ps(r1, r0, _MM_SHUFFLE(1, 2, 2, 2));
+	t1 = _mm_shuffle_ps(t1, t1, _MM_SHUFFLE(1, 3, 2, 0));
+
+	SIMDType temp3 = _mm_add_ps(t0, t1);
+	temp2 = _mm_mul_ps(temp2, sign2);
+
+	t0 = _mm_shuffle_ps(temp1, temp2, _MM_SHUFFLE(0, 0, 1, 0));
+	t1 = _mm_shuffle_ps(temp1, temp3, _MM_SHUFFLE(0, 2, 3, 2));
+	t2 = _mm_shuffle_ps(temp2, temp3, _MM_SHUFFLE(1, 0, 2, 1));
+
+	SIMDType tensor0 = _mm_shuffle_ps(t0, t2, _MM_SHUFFLE(2, 0, 2, 0));
+	SIMDType tensor1 = _mm_shuffle_ps(t0, t2, _MM_SHUFFLE(3, 1, 1, 2));
+	SIMDType tensor2 = _mm_shuffle_ps(t2, t1, _MM_SHUFFLE(2, 0, 1, 0));
+	SIMDType tensor3 = _mm_shuffle_ps(t2, t1, _MM_SHUFFLE(1, 2, 3, 2));
+
+	t0 = _mm_and_ps(r11sr00_le, tensor0);
+	t1 = _mm_andnot_ps(r11sr00_le, tensor1);
+	t0 = _mm_or_ps(t0, t1);
+	t1 = _mm_and_ps(r11pr00_le, tensor2);
+	t2 = _mm_andnot_ps(r11pr00_le, tensor3);
+	t1 = _mm_or_ps(t1, t2);
+	t0 = _mm_and_ps(r22_le, t0);
+	t1 = _mm_andnot_ps(r22_le, t1);
+	t2 = _mm_or_ps(t0, t1);
+
+	t0 = SIMDVec4Length(t2);
+	return _mm_div_ps(t2, t0);
+}
+
+SIMDType SIMDMatrixMulRow(const SIMDType& row, const SIMDType& col1, const SIMDType& col2, const SIMDType& col3, const SIMDType& col4)
+{
+	SIMDType x = _mm_shuffle_ps(row, row, _MM_SHUFFLE(0,0,0,0));
+	SIMDType y = _mm_shuffle_ps(row, row, _MM_SHUFFLE(1,1,1,1));
+	SIMDType z = _mm_shuffle_ps(row, row, _MM_SHUFFLE(2,2,2,2));
+	SIMDType w = _mm_shuffle_ps(row, row, _MM_SHUFFLE(3,3,3,3));
+
+	x = _mm_mul_ps(x, col1);
+	y = _mm_mul_ps(y, col2);
+	z = _mm_mul_ps(z, col3);
+	w = _mm_mul_ps(w, col4);
+
+	x = _mm_add_ps(x, z);
+	y = _mm_add_ps(y, w);
+	x = _mm_add_ps(x, y);
+
+	return x;
+}
+
+SIMDMatrix SIMDMatrixMul(const SIMDMatrix& left, const SIMDMatrix& right)
+{
+	SIMDMatrix result = {{
+		SIMDMatrixMulRow(right.elements[0], left.elements[1], left.elements[2], left.elements[3], left.elements[4]),
+		SIMDMatrixMulRow(right.elements[1], left.elements[1], left.elements[2], left.elements[3], left.elements[4]),
+		SIMDMatrixMulRow(right.elements[2], left.elements[1], left.elements[2], left.elements[3], left.elements[4]),
+		SIMDMatrixMulRow(right.elements[3], left.elements[1], left.elements[2], left.elements[3], left.elements[4])
+	}};
+
+	return result;
+}
+
+SIMDMatrix SIMDMatrixTranspose(const SIMDMatrix& matrix)
+{
+	SIMDMatrix result = {{
+		_mm_shuffle_ps(matrix.elements[0], matrix.elements[1], _MM_SHUFFLE(1, 0, 1, 0)),
+		_mm_shuffle_ps(matrix.elements[2], matrix.elements[3], _MM_SHUFFLE(1, 0, 1, 0)),
+		_mm_shuffle_ps(matrix.elements[0], matrix.elements[1], _MM_SHUFFLE(3, 2, 3, 2)),
+		_mm_shuffle_ps(matrix.elements[2], matrix.elements[3], _MM_SHUFFLE(3, 2, 3, 2))
+	}};
+
+	return result;
+}
+
+SIMDMatrix SIMDMatrixInverse(const SIMDMatrix& matrix)
+{
+	SIMDType col1 = matrix.elements[0];
+	SIMDType col2 = matrix.elements[1];
+	SIMDType col3 = matrix.elements[2];
+	SIMDType col4 = matrix.elements[3];
+
+	SIMDType v00 = _mm_shuffle_ps(col3, col3, _MM_SHUFFLE(1, 1, 0, 0));
+	SIMDType v10 = _mm_shuffle_ps(col4, col4, _MM_SHUFFLE(3, 2, 3, 2));
+	SIMDType v01 = _mm_shuffle_ps(col1, col1, _MM_SHUFFLE(1, 1, 0, 0));
+	SIMDType v11 = _mm_shuffle_ps(col2, col2, _MM_SHUFFLE(3, 2, 3, 2));
+	SIMDType v02 = _mm_shuffle_ps(col3, col1, _MM_SHUFFLE(2, 0, 2, 0));
+	SIMDType v12 = _mm_shuffle_ps(col4, col2, _MM_SHUFFLE(3, 1, 3, 1));
+
+	SIMDType d0 = _mm_mul_ps(v00, v10);
+	SIMDType d1 = _mm_mul_ps(v01, v11);
+	SIMDType d2 = _mm_mul_ps(v02, v12);
+
+	v00 = _mm_shuffle_ps(col3, col3, _MM_SHUFFLE(3, 2, 3, 2));
+	v10 = _mm_shuffle_ps(col4, col4, _MM_SHUFFLE(1, 1, 0, 0));
+	v01 = _mm_shuffle_ps(col1, col1, _MM_SHUFFLE(3, 2, 3, 2));
+	v11 = _mm_shuffle_ps(col2, col2, _MM_SHUFFLE(1, 1, 0, 0));
+	v02 = _mm_shuffle_ps(col3, col1, _MM_SHUFFLE(3, 1, 3, 1));
+	v12 = _mm_shuffle_ps(col4, col2, _MM_SHUFFLE(2, 0, 2, 0));
+
+	v00 = _mm_mul_ps(v00, v10);
+	v01 = _mm_mul_ps(v01, v11);
+	v02 = _mm_mul_ps(v02, v12);
+	d0 = _mm_sub_ps(d0, v00);
+	d1 = _mm_sub_ps(d1, v01);
+	d2 = _mm_sub_ps(d2, v02);
+
+	v11 = _mm_shuffle_ps(d0, d2, _MM_SHUFFLE(1, 1, 3, 1));
+	v00 = _mm_shuffle_ps(col2, col2, _MM_SHUFFLE(1, 0, 2, 1));
+	v10 = _mm_shuffle_ps(v11, d0, _MM_SHUFFLE(0, 3, 0, 2));
+	v01 = _mm_shuffle_ps(col1, col1, _MM_SHUFFLE(0, 1, 0, 2));
+	v11 = _mm_shuffle_ps(v11, d0, _MM_SHUFFLE(2, 1, 2, 1));
+
+	SIMDType v13 = _mm_shuffle_ps(d1, d2, _MM_SHUFFLE(3, 3, 3, 1));
+	v02 = _mm_shuffle_ps(col4, col4, _MM_SHUFFLE(1, 0, 2, 1));
+	v12 = _mm_shuffle_ps(v13, d1, _MM_SHUFFLE(0, 3, 0, 2));
+	SIMDType v03 = _mm_shuffle_ps(col3, col3, _MM_SHUFFLE(0, 1, 0, 2));
+	v13 = _mm_shuffle_ps(v13, d1, _MM_SHUFFLE(2, 1, 2, 1));
+
+	SIMDType c0 = _mm_mul_ps(v00, v10);
+	SIMDType c2 = _mm_mul_ps(v01, v11);
+	SIMDType c4 = _mm_mul_ps(v02, v12);
+	SIMDType c6 = _mm_mul_ps(v03, v13);
+
+	v11 = _mm_shuffle_ps(d0, d2, _MM_SHUFFLE(0, 0, 1, 0));
+	v00 = _mm_shuffle_ps(col2, col2, _MM_SHUFFLE(2, 1, 3, 2));
+	v10 = _mm_shuffle_ps(d0, v11, _MM_SHUFFLE(2, 1, 0, 3));
+	v01 = _mm_shuffle_ps(col1, col1, _MM_SHUFFLE(1, 3, 2, 3));
+	v11 = _mm_shuffle_ps(d0, v11, _MM_SHUFFLE(0, 2, 1, 2));
+
+	v13 = _mm_shuffle_ps(d1, d2, _MM_SHUFFLE(2, 2, 1, 0));
+	v02 = _mm_shuffle_ps(col4, col4, _MM_SHUFFLE(2, 1, 3, 2));
+	v12 = _mm_shuffle_ps(d1, v13, _MM_SHUFFLE(2, 1, 0, 3));
+	v03 = _mm_shuffle_ps(col3, col3, _MM_SHUFFLE(1, 3, 2, 3));
+	v13 = _mm_shuffle_ps(d1, v13, _MM_SHUFFLE(0, 2, 1, 2));
+
+	v00 = _mm_mul_ps(v00, v10);
+	v01 = _mm_mul_ps(v01, v11);
+	v02 = _mm_mul_ps(v02, v12);
+	v03 = _mm_mul_ps(v03, v13);
+	c0 = _mm_sub_ps(c0, v00);
+	c2 = _mm_sub_ps(c2, v01);
+	c4 = _mm_sub_ps(c4, v02);
+	c6 = _mm_sub_ps(c6, v03);
+
+	v00 = _mm_shuffle_ps(col2, col2, _MM_SHUFFLE(0, 3, 0, 3));
+
+	v10 = _mm_shuffle_ps(d0, d2, _MM_SHUFFLE(1, 0, 2, 2));
+	v10 = _mm_shuffle_ps(v10, v10, _MM_SHUFFLE(0, 2, 3, 0));
+	v01 = _mm_shuffle_ps(col1, col1, _MM_SHUFFLE(2, 0, 3, 1));
+
+	v11 = _mm_shuffle_ps(d0, d2, _MM_SHUFFLE(1, 0, 3, 0));
+	v11 = _mm_shuffle_ps(v11, v11, _MM_SHUFFLE(2, 1, 0, 3));
+	v02 = _mm_shuffle_ps(col4, col4, _MM_SHUFFLE(0, 3, 0, 3));
+
+	v12 = _mm_shuffle_ps(d1, d2, _MM_SHUFFLE(3, 2, 2, 2));
+	v12 = _mm_shuffle_ps(v12, v12, _MM_SHUFFLE(0, 2, 3, 0));
+	v03 = _mm_shuffle_ps(col3, col3, _MM_SHUFFLE(2, 0, 3, 1));
+
+	v13 = _mm_shuffle_ps(d1, d2, _MM_SHUFFLE(3, 2, 3, 0));
+	v13 = _mm_shuffle_ps(v13, v13, _MM_SHUFFLE(2, 1, 0, 3));
+
+	v00 = _mm_mul_ps(v00, v10);
+	v01 = _mm_mul_ps(v01, v11);
+	v02 = _mm_mul_ps(v02, v12);
+	v03 = _mm_mul_ps(v03, v13);
+	SIMDType c1 = _mm_sub_ps(c0, v00);
+	c0 = _mm_add_ps(c0, v00);
+	SIMDType c3 = _mm_add_ps(c2, v01);
+	c2 = _mm_sub_ps(c2, v01);
+	SIMDType c5 = _mm_sub_ps(c4, v02);
+	c4 = _mm_add_ps(c4, v02);
+	SIMDType c7 = _mm_add_ps(c6, v03);
+	c6 = _mm_sub_ps(c6, v03);
+
+	c0 = _mm_shuffle_ps(c0, c1, _MM_SHUFFLE(3, 1, 2, 0));
+	c2 = _mm_shuffle_ps(c2, c3, _MM_SHUFFLE(3, 1, 2, 0));
+	c4 = _mm_shuffle_ps(c4, c5, _MM_SHUFFLE(3, 1, 2, 0));
+	c6 = _mm_shuffle_ps(c6, c7, _MM_SHUFFLE(3, 1, 2, 0));
+	c0 = _mm_shuffle_ps(c0, c0, _MM_SHUFFLE(3, 1, 2, 0));
+	c2 = _mm_shuffle_ps(c2, c2, _MM_SHUFFLE(3, 1, 2, 0));
+	c4 = _mm_shuffle_ps(c4, c4, _MM_SHUFFLE(3, 1, 2, 0));
+	c6 = _mm_shuffle_ps(c6, c6, _MM_SHUFFLE(3, 1, 2, 0));
+
+	// dot product
+	SIMDType temp1 = _mm_mul_ps(c0, col1);
+	SIMDType temp2 = col1;
+
+	temp2 = _mm_shuffle_ps(temp2, temp1, _MM_SHUFFLE(1, 0, 0, 0));
+	temp2 = _mm_add_ps(temp2, temp1);
+	temp1 = _mm_shuffle_ps(temp1, temp2, _MM_SHUFFLE(0, 3, 0, 0));
+	temp1 = _mm_add_ps(temp1, temp2);
+
+	SIMDType determinant = _mm_shuffle_ps(temp1, temp1, _MM_SHUFFLE(2, 2, 2, 2));
+	determinant = _mm_div_ps(_mm_set1_ps(1.0f), determinant);
+
+	col1 = _mm_mul_ps(c0, determinant);
+	col2 = _mm_mul_ps(c2, determinant);
+	col3 = _mm_mul_ps(c4, determinant);
+	col4 = _mm_mul_ps(c6, determinant);
+
+	// check for an infinite element
+	SIMDType sign_mask = _mm_set1_ps(0x7FFFFFFF);
+	SIMDType infinity = _mm_set1_ps(0x7F800000);
+
+	c1 = _mm_and_ps(col1, sign_mask);
+	c2 = _mm_and_ps(col2, sign_mask);
+	c3 = _mm_and_ps(col3, sign_mask);
+	c4 = _mm_and_ps(col4, sign_mask);
+
+	c1 = _mm_cmpeq_ps(c1, infinity);
+	c2 = _mm_cmpeq_ps(c2, infinity);
+	c3 = _mm_cmpeq_ps(c3, infinity);
+	c4 = _mm_cmpeq_ps(c4, infinity);
+
+	c1 = _mm_or_ps(c1, c2);
+	c2 = _mm_or_ps(c3, c4);
+	c1 = _mm_or_ps(c1, c2);
+
+	if (_mm_movemask_ps(c1) != 0) {
+		return gMatrixIdentity;
+	}
+
+	temp1 = _mm_shuffle_ps(col1, col2, _MM_SHUFFLE(1, 0, 1, 0));
+	temp2 = _mm_shuffle_ps(col3, col4, _MM_SHUFFLE(1, 0, 1, 0));
+	SIMDType temp3 = _mm_shuffle_ps(col1, col2, _MM_SHUFFLE(3, 2, 3, 2));
+	SIMDType temp4 = _mm_shuffle_ps(col3, col4, _MM_SHUFFLE(3, 2, 3, 2));
+
+	SIMDMatrix result = {{
+		_mm_shuffle_ps(temp1, temp2, _MM_SHUFFLE(2, 0, 2, 0)),
+		_mm_shuffle_ps(temp1, temp2, _MM_SHUFFLE(3, 1, 3, 1)),
+		_mm_shuffle_ps(temp3, temp4, _MM_SHUFFLE(2, 0, 2, 0)),
+		_mm_shuffle_ps(temp3, temp4, _MM_SHUFFLE(3, 1, 3, 1))
+	}};
+
+	return result;
+}
+
+bool SIMDMatrixHasInfiniteElement(const SIMDMatrix& matrix)
+{
+	SIMDType col1 = _mm_and_ps(matrix.elements[0], gSignMask);
+	SIMDType col2 = _mm_and_ps(matrix.elements[1], gSignMask);
+	SIMDType col3 = _mm_and_ps(matrix.elements[2], gSignMask);
+	SIMDType col4 = _mm_and_ps(matrix.elements[3], gSignMask);
+
+	col1 = _mm_cmpeq_ps(col1, gInfinity);
+	col2 = _mm_cmpeq_ps(col2, gInfinity);
+	col3 = _mm_cmpeq_ps(col3, gInfinity);
+	col4 = _mm_cmpeq_ps(col4, gInfinity);
+
+	col1 = _mm_or_ps(col1, col2);
+	col2 = _mm_or_ps(col3, col4);
+	col1 = _mm_or_ps(col1, col2);
+
+	return _mm_movemask_ps(col1) != 0;
+}
+
+bool SIMDMatrixHasNaNElement(const SIMDMatrix& matrix)
+{
+	SIMDType col1 = _mm_cmpneq_ps(matrix.elements[0], matrix.elements[0]);
+	SIMDType col2 = _mm_cmpneq_ps(matrix.elements[1], matrix.elements[1]);
+	SIMDType col3 = _mm_cmpneq_ps(matrix.elements[2], matrix.elements[2]);
+	SIMDType col4 = _mm_cmpneq_ps(matrix.elements[3], matrix.elements[3]);
+
+	col1 = _mm_or_ps(col1, col2);
+	col2 = _mm_or_ps(col3, col4);
+	col1 = _mm_or_ps(col1, col2);
+
+	return _mm_movemask_ps(col1) != 0;
+}
+
+SIMDTransform SIMDTransformConcat(const SIMDTransform& left, const SIMDTransform& right)
+{
+	SIMDTransform result = {
+		SIMDAdd(left.translation, right.translation),
+		SIMDQuatMul(left.rotation, right.rotation),
+		SIMDMul(left.scale, right.scale)
+	};
+
+	return result;
 }
 
 NS_END
