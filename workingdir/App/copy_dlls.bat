a9d2e309521c0c200c455274c5512767cb83ffb2
Connway <2028855+Connway@users.noreply.github.com>
2015-06-24 23:34:07 -0700

    Updated copy_dlls.bat to use relative paths instead of absolute paths.

--- workingdir/App/copy_dlls.bat
+++ workingdir/App/copy_dlls.bat
@@ -1,13 +1,17 @@
-copy F:\Projects\Shibboleth\build\output\x86\Debug\ShibbolethComponents32d.dll Components\ShibbolethComponents32d.dll /Y
-copy F:\Projects\Shibboleth\build\output\x86\Debug\ShibbolethManagers32d.dll Managers\ShibbolethManagers32d.dll /Y
-copy F:\Projects\Shibboleth\build\output\x86\Debug\ShibbolethStates32d.dll States\ShibbolethStates32d.dll /Y
-copy F:\Projects\Shibboleth\build\output\x86\Debug\Graphics_OpenGL32d.dll Graphics_OpenGL32d.dll /Y
-copy F:\Projects\Shibboleth\build\output\x86\Debug\Graphics_Direct3D32d.dll Graphics_Direct3D32d.dll /Y
-copy F:\Projects\Shibboleth\build\output\x86\Debug\ShibbolethRenderPipelines32d.dll "Render Pipelines\ShibbolethRenderPipelines32d.dll" /Y
+mkdir Components
+mkdir Managers
+mkdir "Render Pipelines"
 
-copy F:\Projects\Shibboleth\build\output\x64\Debug\ShibbolethComponents64d.dll Components\ShibbolethComponents64d.dll /Y
-copy F:\Projects\Shibboleth\build\output\x64\Debug\ShibbolethManagers64d.dll Managers\ShibbolethManagers64d.dll /Y
-copy F:\Projects\Shibboleth\build\output\x64\Debug\ShibbolethStates64d.dll States\ShibbolethStates64d.dll /Y
-copy F:\Projects\Shibboleth\build\output\x64\Debug\Graphics_OpenGL64d.dll Graphics_OpenGL64d.dll /Y
-copy F:\Projects\Shibboleth\build\output\x64\Debug\Graphics_Direct3D64d.dll Graphics_Direct3D64d.dll /Y
-copy F:\Projects\Shibboleth\build\output\x64\Debug\ShibbolethRenderPipelines64d.dll "Render Pipelines\ShibbolethRenderPipelines64d.dll" /Y
+copy ..\..\build\output\x86\Debug\ShibbolethComponents32d.dll Components\ShibbolethComponents32d.dll /Y
+copy ..\..\build\output\x86\Debug\ShibbolethManagers32d.dll Managers\ShibbolethManagers32d.dll /Y
+copy ..\..\build\output\x86\Debug\ShibbolethStates32d.dll States\ShibbolethStates32d.dll /Y
+copy ..\..\build\output\x86\Debug\Graphics_OpenGL32d.dll Graphics_OpenGL32d.dll /Y
+copy ..\..\build\output\x86\Debug\Graphics_Direct3D32d.dll Graphics_Direct3D32d.dll /Y
+copy ..\..\build\output\x86\Debug\ShibbolethRenderPipelines32d.dll "Render Pipelines\ShibbolethRenderPipelines32d.dll" /Y
+
+copy ..\..\build\output\x64\Debug\ShibbolethComponents64d.dll Components\ShibbolethComponents64d.dll /Y
+copy ..\..\build\output\x64\Debug\ShibbolethManagers64d.dll Managers\ShibbolethManagers64d.dll /Y
+copy ..\..\build\output\x64\Debug\ShibbolethStates64d.dll States\ShibbolethStates64d.dll /Y
+copy ..\..\build\output\x64\Debug\Graphics_OpenGL64d.dll Graphics_OpenGL64d.dll /Y
+copy ..\..\build\output\x64\Debug\Graphics_Direct3D64d.dll Graphics_Direct3D64d.dll /Y
+copy ..\..\build\output\x64\Debug\ShibbolethRenderPipelines64d.dll "Render Pipelines\ShibbolethRenderPipelines64d.dll" /Y
