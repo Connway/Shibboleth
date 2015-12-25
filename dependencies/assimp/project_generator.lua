6068e6b86e8aaf0f85a0877fc30aa7dc5ca78872
Connway <2028855+Connway@users.noreply.github.com>
2015-12-25 13:00:29 -0800

    Small edits to assimp project generator.

--- dependencies/assimp/project_generator.lua
+++ dependencies/assimp/project_generator.lua
@@ -28,10 +28,10 @@ project "assimp"
 	}
 
 	defines { "ASSIMP_BUILD_BOOST_WORKAROUND", "ASSIMP_BUILD_NO_OWN_ZLIB" }
+	rtti "On"
 
 	filter { "system:windows" }
 		defines { "_CRT_SECURE_NO_WARNINGS", "_SCL_SECURE_NO_WARNINGS" }
-		rtti "On"
 
 	filter {}
 
