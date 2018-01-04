67a5d9760668244a4f50442e47567a925944b908
connway <2028855+Connway@users.noreply.github.com>
2018-01-03 22:12:32 -0800

    Fixed premake errors. Added rpmalloc in preparation for replacing jemalloc.

---
+++ dependencies/rpmalloc/project_generator.lua
@@ -0,0 +1,10 @@
+project "rpmalloc"
+	if _ACTION then
+		location ("../../project/" .. _ACTION .. "/dependencies")
+	end
+
+	kind "StaticLib"
+	language "C++"
+	warnings "Default"
+
+	files { "**.cpp", "**.h", "**.inl" }
