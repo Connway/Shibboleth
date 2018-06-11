14932ea13e62b3c838360c010fb2294333fcad42
connway <2028855+Connway@users.noreply.github.com>
2018-06-10 19:09:57 -0700

    [SHIBBOLETH] Forgot some project files. Started working on ViewportWindow.

---
+++ src/Modules/EditorBasic/Shibboleth_ViewportWindow.cpp
@@ -0,0 +1,67 @@
+/************************************************************************************
+Copyright (C) 2018 by Nicholas LaCroix
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+************************************************************************************/
+
+#include "Shibboleth_ViewportWindow.h"
+#include <Shibboleth_EditorWindowAttribute.h>
+
+SHIB_REFLECTION_EXTERNAL_DEFINE(Shibboleth::ViewportWindow)
+
+SHIB_REFLECTION_BUILDER_BEGIN(Shibboleth::ViewportWindow)
+	.CTOR(wxWindow*, wxWindowID, const wxPoint&, const wxSize&)
+	.CTOR(wxWindow*, wxWindowID, const wxPoint&)
+	.CTOR(wxWindow*, wxWindowID)
+	.CTOR(wxWindow*)
+
+	.BASE(wxWindow)
+
+	.classAttrs(
+		EditorWindowAttribute("&Viewport", "Viewport")
+	)
+
+SHIB_REFLECTION_BUILDER_END(Shibboleth::ViewportWindow)
+
+NS_SHIBBOLETH
+
+wxBEGIN_EVENT_TABLE(ViewportWindow, wxPanel)
+	EVT_SIZE(ViewportWindow::onResize)
+wxEND_EVENT_TABLE()
+
+ViewportWindow::ViewportWindow(
+	wxWindow* parent,
+	wxWindowID id,
+	const wxPoint& pos,
+	const wxSize& size
+):
+	wxPanel(parent, id, pos, size)
+{
+#ifdef PLATFORM_WINDOWS
+	_window.init(GetHWND());
+#endif
+}
+
+void ViewportWindow::onResize(wxSizeEvent& event)
+{
+	const wxSize size = event.GetSize();
+	_window.setDimensions(size.GetWidth(), size.GetHeight());
+}
+
+NS_END
