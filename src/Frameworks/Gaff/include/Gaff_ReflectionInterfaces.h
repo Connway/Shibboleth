b27ed43c05bda584001bb9bf579782cd98e510fb
connway <connway@localhost>
2018-08-24 17:40:21 -0700

    [GAFF] Fixed compilation errors in ReflectionCast and InterfaceCast.
    [SHIBBOLETH]
    Added ECSArchetype::fromJSON().
    Added shared components to ECSArchetype.

--- src/Frameworks/Gaff/include/Gaff_ReflectionInterfaces.h
+++ src/Frameworks/Gaff/include/Gaff_ReflectionInterfaces.h
@@ -1252,13 +1252,13 @@ public:
 template <class Derived, class Base>
 const Derived* ReflectionCast(const Base& object)
 {
-	return object.getReflectionDefinition().getInterface<Derived>(object.getBasePointer());
+	return object.getReflectionDefinition().template getInterface<Derived>(object.getBasePointer());
 }
 
 template <class Derived, class Base>
 Derived* ReflectionCast(Base& object)
 {
-	return object.getReflectionDefinition().getInterface<Derived>(object.getBasePointer());
+	return object.getReflectionDefinition().template getInterface<Derived>(object.getBasePointer());
 }
 
 template <class Derived, class Base>
@@ -1272,30 +1272,6 @@ Derived* InterfaceCast(Base& object, Hash64 interface_name)
 {
 	return reinterpret_cast<Derived*>(object.getReflectionDefinition().getInterface(interface_name, object.getBasePointer()));
 }
-
-//template <class T>
-//const T* ReflectionCast(const IReflectionObject& object)
-//{
-//	return object.getReflectionDefinition().getInterface<T>(object.getBasePointer());
-//}
-//
-//template <class T>
-//T* ReflectionCast(IReflectionObject& object)
-//{
-//	return object.getReflectionDefinition().getInterface<T>(object.getBasePointer());
-//}
-
-//template <class T>
-//const T* InterfaceCast(const IReflectionObject& object, Hash64 interface_name)
-//{
-//	return reinterpret_cast<T*>(object.getReflectionDefinition().getInterface(interface_name, object.getBasePointer()));
-//}
-//
-//template <class T>
-//T* InterfaceCast(IReflectionObject& object, Hash64 interface_name)
-//{
-//	return reinterpret_cast<T*>(object.getReflectionDefinition().getInterface(interface_name, object.getBasePointer()));
-//}
 
 NS_END
 
