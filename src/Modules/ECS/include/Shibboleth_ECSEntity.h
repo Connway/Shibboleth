9b842819c5530085aa0bccf86133133964429182
Connway <Connway@localhost>
2019-02-16 23:13:17 -0800

    [SHIBBOLETH] Still trying to figure stuff out.

--- src/Modules/ECS/include/Shibboleth_ECSEntity.h
+++ src/Modules/ECS/include/Shibboleth_ECSEntity.h
@@ -22,76 +22,11 @@ THE SOFTWARE.
 
 #pragma once
 
-#include <Shibboleth_Vector.h>
-#include <Gaff_Function.h>
-
-NS_GAFF
-	class IReflectionDefinition;
-NS_END
+#include <Shibboleth_Defines.h>
+#include <Gaff_Defines.h>
 
 NS_SHIBBOLETH
 
 using EntityID = int32_t;
-
-class ECSQuery final
-{
-public:
-	template <class T>
-	using TypedFilterFunc = eastl::function<bool (T&)>;
-
-	template <class T>
-	using TypedPushToListFunc = eastl::function<void ()>;
-
-	using PushToListFunc = eastl::function<void ()>;
-	using FilterFunc = eastl::function<bool (void*)>;
-
-	template <class T, class Arg>
-	void addShared(PushToListFunc&& push_to_list, TypedFilterFunc<Arg>&& filter)
-	{
-		auto func = Gaff::Func([filter](void* data) -> bool { return filter(*reinterpret_cast<Arg*>(data)); });
-		addShared(Reflection<T>::GetReflectionDefinition(), std::move(push_to_list), std::move(func));
-	}
-
-	template <class T>
-	void addShared(PushToListFunc&& push_to_list, FilterFunc&& filter)
-	{
-		addShared(Reflection<T>::GetReflectionDefinition(), std::move(push_to_list), std::move(filter));
-	}
-
-	template <class T>
-	void addShared(PushToListFunc&& push_to_list)
-	{
-		addShared(Reflection<T>::GetReflectionDefinition(), std::move(push_to_list));
-	}
-
-	template <class T>
-	void add(PushToListFunc&& push_to_list)
-	{
-		add(Reflection<T>::GetReflectionDefinition(), std::move(push_to_list));
-	}
-
-	void addShared(const Gaff::IReflectionDefinition* ref_def, PushToListFunc&& push_to_list, FilterFunc&& filter);
-	void addShared(const Gaff::IReflectionDefinition* ref_def, PushToListFunc&& push_to_list);
-	void add(const Gaff::IReflectionDefinition* ref_def, PushToListFunc&& push_to_list);
-
-	//bool filter() const;
-
-private:
-	struct QueryDataShared final
-	{
-		const Gaff::IReflectionDefinition* ref_def;
-		PushToListFunc push_to_list;
-		FilterFunc filter;
-	};
-
-	struct QueryData final
-	{
-		const Gaff::IReflectionDefinition* ref_def;
-		PushToListFunc push_to_list;
-	};
-
-	Vector<QueryDataShared> _shared_components;
-	Vector<QueryData> _components;
-};
 
 NS_END
