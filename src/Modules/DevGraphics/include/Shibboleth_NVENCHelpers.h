784e871d548f75e769f2b2939f0d1d74d9785215
Connway <nicholas.lacroix@gmail.com>
2022-02-03 13:49:54 -0800

    Fixing Azure Pipelines to use VS2022. Updating copyright line.

--- src/Modules/DevGraphics/include/Shibboleth_NVENCHelpers.h
+++ src/Modules/DevGraphics/include/Shibboleth_NVENCHelpers.h
@@ -1,5 +1,5 @@
 /************************************************************************************
-Copyright (C) 2021 by Nicholas LaCroix
+Copyright (C) 2022 by Nicholas LaCroix
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
