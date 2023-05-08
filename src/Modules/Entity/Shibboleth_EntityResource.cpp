d6bc116bf87ee47e85353f7dbe8e24bb01fd4529
Connway <nicholas.lacroix@gmail.com>
2023-05-08 09:50:58 -0700

    Forgot to commit some files.

---
+++ src/Modules/Entity/Shibboleth_EntityResource.cpp
@@ -0,0 +1,158 @@
+/************************************************************************************
+Copyright (C) 2023 by Nicholas LaCroix
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+************************************************************************************/
+
+#include "Shibboleth_EntityResource.h"
+#include <Shibboleth_ResourceAttributesCommon.h>
+#include <Shibboleth_ResourceLogging.h>
+#include <Shibboleth_EntityManager.h>
+#include <Shibboleth_AppUtils.h>
+
+namespace
+{
+	static constexpr const char8_t* const k_entity_resource_schema =
+	u8R"({
+		"type": "object",
+
+		"properties":
+		{
+			"name": { "type": "string" },
+
+			"components":
+			{
+				"type": "object",
+				"additionalProperties":
+				{
+					"type": "object",
+
+					"properties":
+					{
+						"type": { "type": "string" },
+						"parent":
+						{
+							"oneOf":
+							[
+								{ "type": "string" },
+								{ "type": "null" }
+							]
+						}
+					},
+
+					"required": ["type"]
+				},
+			}
+		}
+	})";
+}
+
+SHIB_REFLECTION_DEFINE_BEGIN(Shibboleth::EntityResource)
+	.classAttrs(
+		Shibboleth::ResourceExtensionAttribute(u8".entity.bin"),
+		Shibboleth::ResourceExtensionAttribute(u8".entity"),
+		Shibboleth::ResourceSchemaAttribute(k_entity_resource_schema)
+	)
+
+	.template base<Shibboleth::IResource>()
+	.template ctor<>()
+SHIB_REFLECTION_DEFINE_END(Shibboleth::EntityResource)
+
+
+NS_SHIBBOLETH
+
+SHIB_REFLECTION_CLASS_DEFINE(EntityResource)
+
+EntityResource::EntityResource(void):
+	_entity_definition(GetManagerTFast<EntityManager>())
+{
+}
+
+EntityResource::~EntityResource(void)
+{
+}
+
+void EntityResource::load(const ISerializeReader& reader, uintptr_t /*thread_id_int*/)
+{
+	Refl::Reflection<Entity>::GetInstance().load(reader, _entity_definition);
+
+	// Use filename as default name.
+	if (_entity_definition.getName().empty()) {
+		U8String name = getFilePath().getString();
+		const size_t extenstion_start_index = name.rfind(u8'.');
+		const size_t last_separator_index = name.rfind(u8'/');
+
+		GAFF_ASSERT(extenstion_start_index != U8String::npos);
+		GAFF_ASSERT(last_separator_index != U8String::npos);
+		GAFF_ASSERT(last_separator_index < extenstion_start_index);
+
+		name = name.substr(last_separator_index + 1, extenstion_start_index - (last_separator_index + 1));
+
+		_entity_definition.setName(name);
+	}
+
+	const ReflectionManager& refl_mgr = GetApp().getReflectionManager();
+	ProxyAllocator allocator("Resource");
+
+	// Create and load components.
+	{
+		const auto guard = reader.enterElementGuard(u8"components");
+
+		if (reader.isObject()) {
+			reader.forEachInObject([&](const char8_t* comp_name) -> bool
+			{
+				char8_t comp_type[512] = { 0 };
+
+				{
+					const auto type_guard = reader.enterElementGuard(u8"type");
+					reader.readString(comp_type, std::size(comp_type));
+				}
+
+				const Refl::IReflectionDefinition* const ref_def = refl_mgr.getReflection(Gaff::FNV1aHash64String(comp_type));
+
+				if (!ref_def) {
+					LogErrorResource("Failed to fully load entity '%s'. Unknown component type '%s'.", getFilePath().getBuffer(), comp_type);
+					return false;
+				}
+
+				EntityComponent* const component = ref_def->createT<EntityComponent>(allocator);
+
+				if (!component) {
+					LogErrorResource("Failed to fully load entity '%s'. Component type '%s' failed to allocate.", getFilePath().getBuffer(), comp_type);
+					return false;
+				}
+
+				if (!ref_def->load(reader, component->getBasePointer())) {
+					LogErrorResource("Failed to fully load entity '%s'. Component type '%s' failed to load.", getFilePath().getBuffer(), comp_type);
+
+					SHIB_FREET(component, allocator);
+					return false;
+				}
+
+				component->setName(comp_name);
+
+				_entity_definition.addComponent(*component);
+
+				return false;
+			});
+		}
+	}
+}
+
+NS_END
---
+++ src/Modules/Resource/include/Shibboleth_ResourcePtr.h
@@ -0,0 +1,145 @@
+/************************************************************************************
+Copyright (C) 2023 by Nicholas LaCroix
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+************************************************************************************/
+
+#pragma once
+
+#include <Shibboleth_Defines.h>
+#include <Gaff_RefPtr.h>
+
+NS_SHIBBOLETH
+
+class IResource;
+
+template <class T>
+class ResourcePtr final
+{
+public:
+	static_assert(std::is_base_of_v<IResource, T>, "ResourcePtr requires type to derive from IResource.");
+
+	explicit ResourcePtr(T* resource):
+		_resource(resource)
+	{
+	}
+
+	ResourcePtr(void):
+		_resource(nullptr)
+	{
+	}
+
+	ResourcePtr(const ResourcePtr<T>& res_ptr):
+		_resource(res_ptr._resource)
+	{
+	}
+
+	ResourcePtr(ResourcePtr<T>&& res_ptr):
+		_resource(std::move(res_ptr._resource))
+	{
+	}
+
+	bool operator==(const ResourcePtr<T>& rhs) const
+	{
+		return _resource == rhs._resource;
+	}
+
+	bool operator==(const T* rhs) const
+	{
+		return _resource == rhs;
+	}
+
+	bool operator!=(const ResourcePtr<T>& rhs) const
+	{
+		return _resource != rhs._resource;
+	}
+
+	bool operator!=(const T* rhs) const
+	{
+		return _resource != rhs;
+	}
+
+	bool operator<(const T* rhs) const
+	{
+		return _resource < rhs;
+	}
+
+	bool operator>(const T* rhs) const
+	{
+		return _resource > rhs;
+	}
+
+	ResourcePtr<T>& operator=(const ResourcePtr<T>& rhs)
+	{
+		_resource = rhs._resource;
+		return *this;
+	}
+
+	ResourcePtr<T>& operator=(ResourcePtr<T>&& rhs)
+	{
+		_resource = std::move(rhs._resource);
+		return *this;
+	}
+
+	ResourcePtr<T>& operator=(T* rhs)
+	{
+		_resource = rhs;
+		return *this;
+	}
+
+	//const T* operator->(void) const
+	//{
+	//	return _resource;
+	//}
+
+	T* operator->(void) const
+	{
+		return _resource.get();
+	}
+
+	//const T& operator*(void) const
+	//{
+	//	return *_resource;
+	//}
+
+	T& operator*(void) const
+	{
+		return *_resource;
+	}
+
+	operator bool(void) const
+	{
+		return _resource.get() != nullptr;
+	}
+
+	//const T* get(void) const
+	//{
+	//	return _resource;
+	//}
+
+	T* get(void) const
+	{
+		return _resource.get();
+	}
+
+private:
+	Gaff::RefPtr<T> _resource;
+};
+
+NS_END
