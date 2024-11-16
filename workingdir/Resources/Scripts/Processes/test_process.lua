91f2c0adec30cc43c704f0ddf4dfb4f78b1b8c0c
Connway <nicholas.lacroix@gmail.com>
2024-11-16 15:14:17 -0500

    [SHIBBOLETH] Updating some test resources. Adding .clangd.

--- workingdir/Resources/Scripts/Processes/test_process.lua
+++ workingdir/Resources/Scripts/Processes/test_process.lua
@@ -1,6 +1,6 @@
 local TestProcess =
 {
-	entity_id_index = -1,
+	entity_index = -1,
 	rot_speed_index = -1,
 	game_time = nil,
 	ecs_mgr = nil
@@ -8,7 +8,7 @@ local TestProcess =
 
 function TestProcess:init(owner)
 	local variables = owner:getVariables()
-	self.entity_id_index = variables:getVariableIndex("entity_id", Esprit.VariableSet.VariableType.Integer)
+	self.entity_index = variables:getVariableIndex("entity", Esprit.VariableSet.VariableType.Reference)
 	self.rot_speed_index = variables:getVariableIndex("rot_speed", Esprit.VariableSet.VariableType.Float)
 
 	self.game_time = GetManager(Shibboleth.GameTimeManager).game_time
@@ -19,8 +19,12 @@ end
 
 function TestProcess:update(owner, var_inst)
 	local variables = owner:getVariables()
-	local entity_id = variables:getInteger(var_inst, self.entity_id_index)
+	local entity = variables:getReference(var_inst, self.entity_id, Shibboleth.Entity)
 	local rot_speed = variables:getFloat(var_inst, self.rot_speed_index)
+
+	if entity == nil then
+		return
+	end
 
 	-- local pos = Shibboleth.Position.Get(self.ecs_mgr, entity_id)
 	-- pos.value.x = pos.value.x + 0.5 * self.game_time.delta
